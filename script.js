d57871ab7f88195b0886db1afb3ab7a1d74b504e
HaiNam1408 <haiinamm1408@gmail.com>
2024-08-29 17:39:04 +0700

    minit

---
+++ script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML =
+    "Nam iu Uyên ,Nam hứa hongg làm Uyên buồn nữa đouuu :3";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "Không Bao Giờ",
+    "Nam bicc lỗi rồi, sau không dám nữa ạa",
+    "Mong bé tha lỗi choo anhh :((",
+    "Nammm saii rồi , Namm đáng trách ạ",
+    "Bé iu đừng giận nữa nhoo, nhoo nhoo nhoo",
+    "Anhhh iu bé nhắm nhunnn đóoooo",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
